0ee380621dd126e8d08fbe14a6f0f57964e50874
romeo <romeo@romeos-MacBook-Pro.local>
2020-03-01 04:00:48 +0700

    add index update and session tested

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express");
 var request = require("request");
 var cheerio = require("cheerio");
-const app = express();
+let express = require('express'),
+session = require('express-session'),
+FileStore = require('session-file-store')(session),
+app = express();
 const path = require("path");
 const port = 3000;
 
@@ -12,27 +14,88 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-var index_html;
-request('https://www.kingsmanga.net/', function (error, req, html) {
-    if (!error & req.statusCode == 200) {
-        index_html = html;
-    }
-});
+app.use(session({
+    store: new FileStore({
 
+        path: './session-store'
+
+    }),
+    name: '_fs_demo', // cookie will show up as foo site
+    secret: 'imtheshadowiwillfindwhobehidetheshadow',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        // five year cookie
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 5
+    }
+}));
+
+var index_html;
+var bool_html = false;
+function getIndex() {
+    console.log('getindex');
+    request('https://cumanga.com/', function (error, req, html) {
+        if (!error & req.statusCode == 200) {
+            index_html = html;
+            bool_html = true;
+        }
+    });
+}
+getIndex();
+setInterval(getIndex, 300000);
 
 
 app.get('/', function (req, reser) {
+    // reser.send(req.session.text);
     reser.render('index');
 });
 
+// AJAX 
 app.get('/ajax/index/update', async function (req, reser) {
+
     var image = [];
-    try {
-        $('div.post-body-alignment').each(function (i, elem) {
-            image[i] = $(this).text();
-        });
-    } catch (error) {
-        console.log(error);
+    var name = [];
+    var ep = [];
+    var url = [];
+    if (bool_html) {
+        var $ = await cheerio.load(index_html);
+        try {
+            console.log($("style").length);
+            for (var i = 0; i < 4; i++) {
+                var one = ($("style").eq(i).html().substr($("style").eq(i).html().indexOf("'"), $("style").eq(i).html().length));
+                image[i] = one.substr(1, one.indexOf("')") - 1);
+                name[i] = $(".doujin_name").eq(i).text();
+                ep[i] = $('.doujin_t_red').eq(i).text();
+                url[i] = $('.col-6').eq(i).attr("href");
+            }
+            console.log(name);
+            reser.render('ajax/index/update', { name: name, image: image, ep: ep, url: url });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+app.get('/ajax/index/updateall', async function (req, reser) {
+    var image = [];
+    var name = [];
+    var ep = [];
+    var url = [];
+    if (bool_html) {
+        var $ = await cheerio.load(index_html);
+        try {
+            console.log($("style").length);
+            for (var i = 0; i < $("style").length; i++) {
+                var one = ($("style").eq(i).html().substr($("style").eq(i).html().indexOf("'"), $("style").eq(i).html().length));
+                image[i] = one.substr(1, one.indexOf("')") - 1);
+                name[i] = $(".doujin_name").eq(i).text();
+                ep[i] = $('.doujin_t_red').eq(i).text();
+                url[i] = $('.col-6').eq(i).attr("href");
+            }
+            console.log(name);
+            reser.render('ajax/index/updateall', { name: name, image: image, ep: ep, url: url });
+        } catch (error) {
+            console.log(error);
+        }
     }
 });
 
