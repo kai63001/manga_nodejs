73e06fdb1b7ec1e2f4189e7ca51c7ea85f60dda1
romeo <romeo@romeos-MacBook-Pro.local>
2020-02-29 20:14:38 +0700

    sec comit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 const app = express();
 const path = require("path");
-const port = 30000;
+const port = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -12,10 +12,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',function(req,reser){
-    
+var index_html;
+request('https://www.kingsmanga.net/', function (error, req, html) {
+    if (!error & req.statusCode == 200) {
+        index_html = html;
+    }
 });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`);
+
+
+app.get('/', function (req, reser) {
+    reser.render('index');
 });
+
+app.get('/ajax/index/update', async function (req, reser) {
+    var image = [];
+    try {
+        $('div.post-body-alignment').each(function (i, elem) {
+            image[i] = $(this).text();
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(port, function () {
+    console.log(`Runnn progame at port : ${port}!`);
+});
+
+module.exports = app;
